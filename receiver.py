747a9810ce1d62fbb3433c7ccbb8b46d241c5456
izzeldinHajqasem <124907824+izzeldinHajqasem@users.noreply.github.com>
2023-02-14 16:10:26 +0200

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -1,11 +1,15 @@
 # Write your code here :-)
 # Add your Python code here. E.g.
 from microbit import *
+from ssd1306 import initialize, clear_oled 
+from ssd1306_text import add_text
 import radio
 radio.config(group=10)
 radio.on()
-
+initialize()
 while True:
+    clear_oled()
     receive = radio.receive()
     if receive == "warning":
-        display.scroll("warning")
+        add_text(0,3,"warning")
+    sleep(2000)
